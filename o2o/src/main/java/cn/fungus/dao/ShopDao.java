901a07d79a46497d3ab39d32934b6eabb10e9069
fungi8 <fungi.wj@gmail.com>
2019-01-15 15:54:31 +0800

    新增shop和更新shop的功能

--- o2o/src/main/java/cn/fungus/dao/ShopDao.java
+++ o2o/src/main/java/cn/fungus/dao/ShopDao.java
@@ -11,7 +11,14 @@ public interface ShopDao {
     /**
      * 新增店铺
      * @param shop
-     * @return
+     * @return 影响行数
      */
     int insertShop(Shop shop);
+
+    /**
+     * 更新店铺
+     * @param shop
+     * @return
+     */
+    int updateShop(Shop shop);
 }
--- o2o/src/test/java/cn/fungus/dao/ShopDaoTest.java
+++ o2o/src/test/java/cn/fungus/dao/ShopDaoTest.java
@@ -5,8 +5,13 @@ import cn.fungus.entity.Area;
 import cn.fungus.entity.PersonInfo;
 import cn.fungus.entity.Shop;
 import cn.fungus.entity.ShopCategory;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * create by fungus on 2018/6/29
@@ -17,17 +22,38 @@ public class ShopDaoTest extends BaseTest {
     private ShopDao shopDao;
 
     @Test
+    @Ignore
     public void testInsertShop() {
         Shop shop = new Shop();
         PersonInfo owner = new PersonInfo();
         Area area = new Area();
         ShopCategory shopCategory = new ShopCategory();
-
         owner.setUserId(1L);
         area.setAreaId(2L);
         shopCategory.setShopCategoryId(1L);
-
-//        shop;
+        shop.setArea(area);
+        shop.setOwner(owner);
+        shop.setShopCategory(shopCategory);
+        shop.setShopName("测试的店铺");
+        shop.setPhone("test");
+        shop.setShopImg("test");
+        shop.setShopAddr("test");
+        shop.setShopDesc("test");
+        shop.setCreateTime(new Date());
+        shop.setAdvice("审核中");
+        shop.setEnableStatus(1);
+        int effectedNum = shopDao.insertShop(shop);
+        assertEquals(1, effectedNum);
     }
 
+    @Test
+    public void testUpdateShop() {
+        Shop shop = new Shop();
+        shop.setShopId(1L);
+        shop.setShopAddr("测试更新地址");
+        shop.setShopDesc("测试更新描述");
+        shop.setLastEditTime(new Date());
+        int effectedNum = shopDao.updateShop(shop);
+        assertEquals(1, effectedNum);
+    }
 }
